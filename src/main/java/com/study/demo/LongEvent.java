6caa42dec359ad38fe6195a44c5e5d31c8100609
Administrator <renlian2010@sina.com>
2021-05-05 17:57:24 +0800

    2021/05/05 disruptor第一次提交

---
+++ src/main/java/com/study/demo/LongEvent.java
@@ -0,0 +1,17 @@
+package com.study.demo;
+
+public class LongEvent {
+
+    private Long value;
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "LongEvent{" +
+                "value=" + value +
+                '}';
+    }
+}
